2acc95cd4e9ae4ebc585fa851c63017073549f36
ShreyaSharma24 <104619602+ShreyaSharma24@users.noreply.github.com>
2024-08-18 22:58:56 +0530

    Add files via upload

---
+++ ShreyaProject/src/test/java/Part3_Package/FormPageTest.java
@@ -0,0 +1,32 @@
+package Part3_Package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FormPageTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.34.0-win64\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/automation-practice-form");
+        FormPage formPage = new FormPage(driver);
+        formPage.fillFirstName("Shreya");
+        formPage.fillLastName("Sharma");
+        formPage.fillEmail("shreya24@gmail.com");
+        formPage.selectGender();
+        formPage.fillMobileNumber("1234567890");
+        formPage.selectDateOfBirth();
+        try {
+            formPage.fillSubjects("Physics", "English");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        formPage.selectHobbies();
+        formPage.uploadImage("D:\\images_for _my _proj\\andaman.jpg");
+        formPage.fillCurrentAddress("110, Jalandhar");
+        formPage.selectState("Haryana");
+        formPage.selectCity("Panipat");
+        formPage.submitForm();
+
+    }
+}
